505185d6af9fc947c14c17221192869c04d48182
Elias Engström <elias.engstrom@elev.ga.ntig.se>
2024-08-26 14:34:33 +0200

    Lite inställningar och Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Tjena, Världen!");
+Console.WriteLine("Nu mitt första C#-program");
+Console.Write("Vad gör du på fritiden? ");
+string fritiden = Console.ReadLine();
+Console.WriteLine(fritiden +"? Helt framåtlutad och sigmit!");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.BackgroundColor = ConsoleColor.Black; 
+Console.WriteLine("Såg du på OS i sommar?");
+Console.WriteLine("Vilken sport gillar du mest?");
