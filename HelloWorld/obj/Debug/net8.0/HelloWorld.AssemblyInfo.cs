332b9e8db6e6d5b931f3bdcd196631adb6942b21
Elias Engström <elias.engstrom@elev.ga.ntig.se>
2024-08-28 14:43:22 +0200

    Träna på cw och cr och Lucktext. Några if/else också.

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5602f38c7a0a0994f7f3d97f8e4e638ac7f3a0aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce8e829c32417e4c8df1228290ee8ccebf8b93d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Kapitel-1/Lucktext/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Ange ett Namn: ");
+string namn = Console.ReadLine();
+Console.Write("Ange ett kroppsdel: ");
+string kroppsdel = Console.ReadLine();
+Console.Write("Ange en prillasmak: ");
+string prillasmak = Console.ReadLine();
+Console.Write("Ange ett en siffra: ");
+string siffra = Console.ReadLine();
+Console.Clear();
+
+
+if (namn == "" || kroppsdel == "" || prillasmak == "" || siffra == "")
+{
+    Console.WriteLine("Naj nu blev fel tror jag");
+}
+else 
+{
+    Console.WriteLine($"Tjena {namn}, visst är {kroppsdel} smarrig, nästan lika god som {prillasmak}. Du ger den en {siffra}, vad nice!");
+}
