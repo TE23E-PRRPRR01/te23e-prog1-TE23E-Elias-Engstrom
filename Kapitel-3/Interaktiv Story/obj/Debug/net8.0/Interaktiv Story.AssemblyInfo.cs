0ee882cbd9178e4e54e0e7ad6d76fed2a3229ed0
Elias Engström <elias.engstrom@elev.ga.ntig.se>
2024-09-04 14:50:50 +0200

    Jag har gibbat lite

--- Kapitel-3/Interaktiv Story/obj/Debug/net8.0/Interaktiv Story.AssemblyInfo.cs
+++ Kapitel-3/Interaktiv Story/obj/Debug/net8.0/Interaktiv Story.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interaktiv Story")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b28172a45c08895e1215ebb45c8cce5770d18a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c12ea42b964febe5fe1dd9f6fdcc6d16b9e0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interaktiv Story")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interaktiv Story")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
