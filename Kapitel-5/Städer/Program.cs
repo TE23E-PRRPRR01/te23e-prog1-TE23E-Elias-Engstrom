9981d66c82e1bf540508cbf3464ff2afe4e97ff2
Elias Engström <eliasengstrom1234@gmail.com>
2024-10-02 10:44:23 +0200

    övat på listor

--- Kapitel-5/Städer/Program.cs
+++ Kapitel-5/Städer/Program.cs
@@ -3,7 +3,7 @@ Console.Clear();
 Console.WriteLine("Här kommer två slumpade städer: ");
 
 // Skapar en lista med städerna
-List<string> städer = ["Stockholm", "Borås", "Göteborg", "Malmö", "Riyad", "Jönköping", "Örnsköldsvik", "Luleå", "Sundsvall", "Motala"];
+List<string> städer = ["Stockholm", "Borås", "Göteborg", "Malmö", "Riyad", "Jönköping", "Örnsköldsvik", "Luleå", "Sundsvall", "Motala", "Västerås"];
 
 int antal = 2;
 while (antal > 0)
