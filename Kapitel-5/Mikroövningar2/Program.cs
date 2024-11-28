fd1a5015abae2426de800afc482e3198b737db42
Elias Engström <eliasengstrom1234@gmail.com>
2024-11-28 13:12:19 +0100

    Tränat på listor

---
+++ Kapitel-5/Mikroövningar2/Program.cs
@@ -0,0 +1,19 @@
+﻿// En lista med 5 namn
+Console.Clear();
+
+List<string> namnLista = ["Elias", "Johnny", "Filip", "Viktor", "Bolin"];
+List<string> orterLista = ["Tullinge", "Huddinge", "Järfälla", "Tumba"];
+List<int> årtalLista = [1945, 2007, 2016, 2020, 2024];
+
+Console.WriteLine($"Min kompisar är: {string.Join(", ", namnLista)}");
+Console.Write("Ett namn som du kollar om redan finns eller lägga till den i listan: ");
+string nyttNamn = Console.ReadLine();
+if (namnLista.Contains(nyttNamn))
+{
+    Console.WriteLine($"{nyttNamn} finns redan i listan");
+}
+else
+{
+    namnLista.Add(nyttNamn);
+    Console.WriteLine(string.Join(", ", namnLista));
+}
