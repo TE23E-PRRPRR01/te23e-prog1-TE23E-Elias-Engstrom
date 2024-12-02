44fc032439bbded747d8b4e7113f7ac8472417cc
Elias Engström <eliasengstrom1234@gmail.com>
2024-12-02 14:52:34 +0100

    Börjat på bordsplaceringar

---
+++ Kapitel-5/Bordsbokning/Program.cs
@@ -0,0 +1,61 @@
+﻿// Labb Bordsbokning    
+Console.Clear();
+
+Console.WriteLine("Detta är Centralrestaurangens bordshanterare");
+
+
+List<(int antal, string namn)> bordData = new List<(int, string)>
+{
+    (0, ""),
+    (0, ""),
+    (0, ""),
+    (0, ""),
+    (0, ""),
+    (0, ""),
+    (0, ""),
+    (0, ""),
+};
+
+string meny = """
+1. Visa alla bord
+2. Ändra bordsinformation
+3. Markera att ett bord är ledigt
+4. Avsluta programmet 
+""";
+
+while (true)
+{
+    Console.WriteLine(meny);
+    Console.Write("Välj ett alternativ: ");
+    string val = Console.ReadLine();
+
+    if (val == "1")
+    {
+        foreach ((int, string) i in bordData)
+        {
+            if (string.IsNullOrWhiteSpace(i.namn))
+            {
+                
+            }
+            Console.WriteLine($"Bord {i} - " );
+        }
+    }
+    else if (val == "2")
+    {
+
+    }
+    else if (val == "3")
+    {
+
+    }
+    else if (val == "4")
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Felaktig inmatning, tryck på valfri knapp för att gå vidare");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
