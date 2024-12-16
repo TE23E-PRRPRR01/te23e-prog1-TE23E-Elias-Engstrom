6153dc115ed192ad4fd2725eca761303b1c8860c
Elias Engström <eliasengstrom1234@gmail.com>
2024-12-16 14:45:49 +0100

    Gjort mikroovningar listor 4.4

---
+++ Kapitel-5/Listor2/Program.cs
@@ -0,0 +1,85 @@
+﻿// Skapa en lista med action-spel
+Console.Clear();
+
+List<string> listaActionSpel = [];
+List<string> listaÄventyrSpel = [];
+
+Console.WriteLine("""
+Vilken lista vill du lägga till spel i.
+1. Action
+2. Äventyr
+3. Båda
+""");
+string val = Console.ReadLine();
+
+if (val == "1")
+{
+    Console.Write("Nu ska du fylla i hur många spel du vill ha i listan: ");
+    string Spel = Console.ReadLine();
+    int antalSpel = int.Parse(Spel);
+    for (var i = 0; i < antalSpel; i++)
+    {
+        Console.Write("Ange ett action-spel: ");
+        string actionSpel = Console.ReadLine();
+        listaActionSpel.Add(actionSpel);
+    }
+
+    foreach (var actionSpel in listaActionSpel)
+    {
+        Console.WriteLine($"- {actionSpel}");
+    }
+}
+else if (val == "2")
+{
+    Console.Write("Nu ska du fylla i hur många spel du vill ha i listan: ");
+    string Spel = Console.ReadLine();
+    int antalSpel = int.Parse(Spel);
+    for (var i = 0; i < antalSpel; i++)
+    {
+        Console.Write("Ange ett äventyr-spel: ");
+        string äventyrSpel = Console.ReadLine();
+        listaÄventyrSpel.Add(äventyrSpel);
+    }
+
+    foreach (var äventyrSpel in listaÄventyrSpel)
+    {
+        Console.WriteLine($"- {äventyrSpel}");
+    }
+}
+else if(val == "3")
+{
+    Console.Write("Nu ska du fylla i hur många action-spel du vill ha i listan: ");
+    string Spel = Console.ReadLine();
+    int antalSpel = int.Parse(Spel);
+    for (var i = 0; i < antalSpel; i++)
+    {
+        Console.Write("Ange ett action-spel: ");
+        string actionSpel = Console.ReadLine();
+        listaActionSpel.Add(actionSpel);
+    }
+    Console.Write("Nu ska du fylla i hur många äventyr-spel du vill ha i listan: ");
+    string Spel1 = Console.ReadLine();
+    int antalSpel1 = int.Parse(Spel1);
+    for (var i = 0; i < antalSpel1; i++)
+    {
+        Console.Write("Ange ett äventyr-spel: ");
+        string äventyrSpel = Console.ReadLine();
+        listaÄventyrSpel.Add(äventyrSpel);
+    }
+
+    Console.WriteLine("Action spelen:");
+    foreach (var actionSpel in listaActionSpel)
+    {
+        Console.WriteLine($"- {actionSpel}");
+    }
+
+     Console.WriteLine("Äventyr spelen:");
+     foreach (var äventyrSpel in listaÄventyrSpel)
+    {
+        Console.WriteLine($"- {äventyrSpel}");
+    }
+
+
+}
+
+
