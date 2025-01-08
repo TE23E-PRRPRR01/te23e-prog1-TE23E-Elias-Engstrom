09c704a8d67255e6d695e4eb1cd6ccd34f07e17b
Elias Engström <eliasengstrom1234@gmail.com>
2025-01-08 15:03:15 +0100

    Tränat på metoder och testat metoder

---
+++ Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,37 @@
+﻿/* **********************************************************************
+*                               M A I N
+ ********************************************************************** */
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+SägHejNamn("Valentin");
+
+TalIKvadrat(5);
+TalIKvadrat(7);
+TalIKvadrat(1);
+TalIKvadrat(8);
+TalIKvadrat(9);
+
+/* **********************************************************************
+*                             M E T O D E R
+ ********************************************************************** */
+
+/// <summary>
+/// Skriv ut hälsning
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Hej på dig! {namn}");
+
+}
+
+/// <summary>
+/// Metod som kvadrerar tal
+/// </summary>
+/// <param name="tal">Nummret</param>
+static void TalIKvadrat(double tal)
+{
+    Console.WriteLine(tal*tal);
+}
---
+++ Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿/* **********************************************************************
+*                               M A I N
+ ********************************************************************** */
+
+Console.Clear();
+Console.WriteLine("Hej Metoder!");
+
+// Anropa metoden
+//SägHej();
+
+// Anropa metoden meny
+//VisaMeny();
+SkriverUtEttMeddelande();
+
+
+/* **********************************************************************
+*                             M E T O D E R
+ ********************************************************************** */
+
+/// <summary>
+/// Metod för att visa en meny
+/// </summary>
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Skriv ut uppgifterna
+    4. Avsluta
+    """);
+
+}
+
+/// <summary>
+/// Metod för att skriva ut ett valfritt meddelande
+/// </summary>
+static void SkriverUtEttMeddelande()
+{
+    Console.WriteLine("Vad vill du skriva ut i consolen?");
+    string meddelande = Console.ReadLine();
+    Console.WriteLine(meddelande);
+}
+
+/// <summary>
+/// Metod som skriver ut en hälsning
+/// </summary>
+static void SägHej()
+{
+    Console.WriteLine("Hej på dig!");
+}
